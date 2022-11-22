25de8429f34e17368bd38257ab2f4d44b19765d0
Bartosz <bartekg001@gmail.com>
2022-11-22 22:55:49 +0100

    Introduce required parameters.

--- Discs/Discs/src/ofApp.h
+++ Discs/Discs/src/ofApp.h
@@ -50,7 +50,7 @@ class ofApp : public ofBaseApp{
 			ofColor color = { 0, 0, 0 };
 			float mass = 0;
 			ofVec2f velocity =  { 1, 1 };
-			ofVec2f acceleration = { 1, 1 };
+			ofVec2f acceleration = { 0, 0 };
 
 			disk_parameters()
 			{
@@ -60,11 +60,11 @@ class ofApp : public ofBaseApp{
 				color[0] = ofRandom(0, 255);
 				color[1] = ofRandom(0, 255);
 				color[2] = ofRandom(0, 255);
-				mass = ofRandom(0.1, 5.0);
+				mass = ofRandom(90.0, 100.0);
 				velocity[0] = ofRandom(-3.0, 3.0);
 				velocity[1] = ofRandom(-3.0, 3.0);
-				acceleration[0] = ofRandom(-1.0, 1.0);
-				acceleration[1] = ofRandom(-1.0, 1.0);
+				acceleration[0] = ofRandom(-0.5, 0.5);
+				acceleration[1] = ofRandom(-0.5, 0.5);
 			}
 		};
 		std::vector<disk_parameters> parameters;
