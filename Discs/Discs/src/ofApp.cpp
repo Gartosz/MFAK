63bebecc583c0479422dbecec44b23daa867fb04
Bartosz <bartekg001@gmail.com>
2022-11-23 23:02:02 +0100

    Remove unused methods.

--- Discs/Discs/src/ofApp.cpp
+++ Discs/Discs/src/ofApp.cpp
@@ -71,57 +71,7 @@ void ofApp::draw() {
 	gui.draw();
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button) {
 	force_points.push_back(ofVec2f(x, y));
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo) {
-
-}
