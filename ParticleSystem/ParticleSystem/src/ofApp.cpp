7f04803f4a3583376f358ed5c969e235c0c7dfb6
Bartosz <bartekg001@gmail.com>
2022-12-06 19:30:50 +0100

    Copy solution from Disks to have base to work on.

--- ParticleSystem/ParticleSystem/src/ofApp.cpp
+++ ParticleSystem/ParticleSystem/src/ofApp.cpp
@@ -1,71 +1,97 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-
+void ofApp::setup() {
+	gui.setup();
+	gui.setSize(250, 120);
+	gui.add(delta_time.setup("Speed", 1.00, 0.00, 2.00));
+	gui.add(gravity.setup("Gravity", 9.81, 0.0, 20.0));
+	gui.add(attractor_mass.setup("Mass", 10.0, 0.0, 300.0));
+	gui.add(viscosity.setup("Viscosity", 0.000181, 0.0, 0.01));
+	gui.add(apply_attractor.setup("Toggle attractor force", false, 20, 20));
+	gui.add(apply_viscosity.setup("Toggle drag force", false, 20, 20));
 }
 
-//--------------------------------------------------------------
-void ofApp::update(){
-
+void ofApp::applyDragForce(ofApp::disk_parameters const &disk, ofVec2f& acceleration)
+{
+	ofVec2f drag_force = -6 * PI * disk.velocity * viscosity * disk.size;
+	acceleration += drag_force / disk.mass;
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
 
+void ofApp::applyAttractorForce(ofApp::disk_parameters const &disk, ofVec2f& acceleration)
+{
+	for (auto& force_point : force_points)
+	{
+		float distance = force_point.distance(disk.pos);
+		ofVec2f distance_vector(force_point - disk.pos);
+		ofVec2f force = (gravity * attractor_mass * disk.mass * distance_vector) / pow(distance * distance + epsilon, float(3 / 2));
+		acceleration += force / disk.mass;
+	}
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::moveDisk(ofApp::disk_parameters& disk, ofVec2f const &acceleration, float const *windowSize)
+{
+	disk.velocity += acceleration * delta_time;
+	disk.pos += disk.velocity * delta_time;
+	checkWallHit(disk, windowSize);
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::update() {
+	const float windowSize[2] = { ofGetWidth(), ofGetHeight() };
+	for (auto& disk : disks)
+	{
+		ofVec2f acceleration(0, 0);
+		if (apply_viscosity)
+			applyDragForce(disk, acceleration);
+		if (apply_attractor)
+			applyAttractorForce(disk, acceleration);
+		moveDisk(disk, acceleration, windowSize);
+	}
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
+float ofApp::checkValueChange(float const &before_modification, float const& pos)
+{
+	return before_modification == pos ? 1.0 : -1.0;
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::checkWallHit(disk_parameters& parameter, float const* windowSize)
+{
+	ofVec2f before_modification(parameter.pos);
+	for (int i = 0; i < 2; ++i)
+	{
+		parameter.pos[i] = ofClamp(parameter.pos[i], parameter.size, windowSize[i] - parameter.size);
+		parameter.velocity[i] *= checkValueChange(before_modification[i], parameter.pos[i]);
+	}
 }
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
+void ofApp::drawDisks()
+{
+	for (auto& disk : disks)
+	{
+		ofSetColor(disk.color);
+		ofDrawCircle(disk.pos, disk.size);
+	}
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
+void ofApp::drawAttractors()
+{
+	for (auto& force_point : force_points)
+	{
+		ofSetColor(ofColor::black);
+		ofDrawCircle(force_point, force_point_size + 3);
+		ofSetColor(force_point_color);
+		ofDrawCircle(force_point, force_point_size);
+	}
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
+void ofApp::draw() {
+	drawDisks();
+	drawAttractors();
+	gui.draw();
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
+void ofApp::mousePressed(int x, int y, int button) {
+	force_points.push_back(ofVec2f(x, y));
 }
 
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
--- ParticleSystem/ParticleSystem/src/ofApp.h
+++ ParticleSystem/ParticleSystem/src/ofApp.h
@@ -1,24 +1,80 @@
 #pragma once
 
 #include "ofMain.h"
+#include <vector>
+#include <ctime>
+#include <stdio.h>
+#include "ofXGui.h"
 
 class ofApp : public ofBaseApp{
 
 	public:
+		ofApp(int n = 1000, float epsilon = 2.0)
+		{
+			ofSetCircleResolution(100);
+			number = n;
+			for (int i = 0; i < n; i++)
+			{
+				disks.push_back(disk_parameters());
+			}
+			force_point_color.set(255, 255, 255);
+			force_point_size = 15;
+			force_points.push_back(ofVec2f(ofGetWidth() / 2, ofGetHeight() / 2));
+			this->epsilon = epsilon;
+		}
+
+	private:
+		int number;
+		struct disk_parameters
+		{
+			int size = 1;
+			ofVec2f pos = { 0, 0 };
+			ofColor color = { 0, 0, 0 };
+			float mass = 0;
+			ofVec2f velocity =  { 1, 1 };
+
+			disk_parameters()
+			{
+				size = std::rand() % 15 + 5;
+				pos[0] = std::rand() % (ofGetWidth() - size * 2);
+				pos[1] = std::rand() % (ofGetHeight() - size * 2);
+				color[0] = ofRandom(0, 240);
+				color[1] = ofRandom(0, 240);
+				color[2] = ofRandom(0, 240);
+				mass = ofRandom(50.0, 150.0);
+				velocity[0] = ofRandom(-5.0, 5.0);
+				velocity[1] = ofRandom(-5.0, 5.0);
+			}
+		};
+		ofxPanel gui;
+
+		ofxFloatSlider delta_time;
+		ofxFloatSlider gravity;
+		ofxFloatSlider attractor_mass;
+		ofxFloatSlider viscosity;
+		ofxToggle apply_attractor;
+		ofxToggle apply_viscosity;
+
+
+		int force_point_size;
+		float epsilon;
+		ofColor force_point_color;
+
+		std::vector<disk_parameters> disks;
+		std::vector<ofVec2f> force_points;
+
+
 		void setup();
 		void update();
 		void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+
+		float checkValueChange(float const& before_modification, float const& pos);
+		void checkWallHit(disk_parameters& parameter, float const* windowSize);
+		void drawAttractors();
+		void drawDisks();
+		void applyDragForce(ofApp::disk_parameters const &disk, ofVec2f& acceleration);
+		void applyAttractorForce(ofApp::disk_parameters const &disk, ofVec2f& acceleration);
+		void ofApp::moveDisk(ofApp::disk_parameters& disk, ofVec2f const& acceleration, float const* windowSize);
 };
