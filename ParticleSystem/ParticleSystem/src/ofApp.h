a018f223761bec0f7d1f6426b1b2496607e3f4ef
Gartosz <bartekg001@gmail.com>
2022-12-23 00:57:20 +0100

    Add floor as ofapp member.

--- ParticleSystem/ParticleSystem/src/ofApp.h
+++ ParticleSystem/ParticleSystem/src/ofApp.h
@@ -10,18 +10,20 @@
 class ofApp : public ofBaseApp{
 
 	public:
-		ofApp(int n = 1000, int _dt = 5)
+		ofApp(int n = 1000, int _dt = 5, float _floor = 0)
 		{
-			ofSetCircleResolution(100);
-			system = ParticleSystem(n, -200);
-			createEmitters();
 			dt = _dt;
+			floor_level = _floor;
+			ofSetCircleResolution(100);
+			system = ParticleSystem(n, floor_level);
+			createEmitters();
 		}
 
 	private:
 		ParticleSystem system;
 		int number;
 		int dt;
+		float floor_level;
 		ofEasyCam camera;
 
 		void update();
