d3daff83fef9ab007b799eaa12fbd52993ec480e
Gartosz <bartekg001@gmail.com>
2022-12-22 01:24:11 +0100

    Update velocity and position in update.

--- ParticleSystem/ParticleSystem/ParticleSystem.cpp
+++ ParticleSystem/ParticleSystem/ParticleSystem.cpp
@@ -56,6 +56,8 @@ void ParticleSystem::update(double dt)
     }
     for (int i = 0; i < data.last_alive_id; ++i)
     {
+        data.particles[i].velocity.y -= data.particles[i].gravity * dt;
+        data.particles[i].pos += data.particles[i].velocity * dt;
         if ((data.particles[i].time_to_live -= dt) <= 0)
             data.kill(i);
     }
