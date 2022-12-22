41e3860dd7c722da246dee1715d7016879ec5078
Gartosz <bartekg001@gmail.com>
2022-12-23 00:57:58 +0100

    Rename emitter to snow.

--- ParticleSystem/ParticleSystem/src/ofApp.cpp
+++ ParticleSystem/ParticleSystem/src/ofApp.cpp
@@ -26,12 +26,13 @@ void ofApp::draw() {
 	camera.end();
 }
 
-void ofApp::createEmitters() {
-	auto particleEmitter = std::make_shared<ParticleEmitter>((float)15 * 0.45f, 1000, 0.001);
+void ofApp::createEmitters() 
+	{
+	auto snowEmitter = std::make_shared<ParticleEmitter>((float)15 * 0.45f, 1000, 0.001);
 	{
 		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(300, 5, 300, 0));
 		posGenerator->pos = glm::vec4{ 0.0, 200.0, 0.0, 0.0 };
-		particleEmitter->addGenerator(posGenerator);
+		snowEmitter->addGenerator(posGenerator);
 	}
-	system.addEmitter(particleEmitter);
+	system.addEmitter(snowEmitter);
 }
