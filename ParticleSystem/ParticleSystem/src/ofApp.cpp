27fef28006a104b6922ce8168241343f2abf7c52
Gartosz <bartekg001@gmail.com>
2022-12-23 01:09:40 +0100

    Modify flame.

--- ParticleSystem/ParticleSystem/src/ofApp.cpp
+++ ParticleSystem/ParticleSystem/src/ofApp.cpp
@@ -36,13 +36,13 @@ void ofApp::createEmitters()
 	}
 	system.addEmitter(snowEmitter);
 
-	auto flameEmitter = std::make_shared<ParticleEmitter>((float)1, 1000, 0.001);
+	auto flameEmitter = std::make_shared<ParticleEmitter>((float)1, 500, 0.002);
 	{
 		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(10, 5, 10, 0));
 		posGenerator->pos = glm::vec4{ 0.0, floor_level, 0.0, 0.0 };
 		posGenerator->color = ofColor::orangeRed;
-		flameEmitter->init_velocity_range[0] = glm::vec4(-0.1, 0.25, -0.1, 0);
-		flameEmitter->init_velocity_range[1] = glm::vec4(0.1, 0.7, 0.1, 0);
+		flameEmitter->init_velocity_range[0] = glm::vec4(-0.15, 0.25, -0.15, 0);
+		flameEmitter->init_velocity_range[1] = glm::vec4(0.15, 0.8, 0.15, 0);
 		flameEmitter->addGenerator(posGenerator);
 	}
 	system.addEmitter(flameEmitter);
