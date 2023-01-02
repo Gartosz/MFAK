160c1792e9da1e2a6a399484fa4161153b65a9d8
Gartosz <bartekg001@gmail.com>
2023-01-02 23:36:33 +0100

    Add smoke.

--- ParticleSystem/ParticleSystem/src/ofApp.cpp
+++ ParticleSystem/ParticleSystem/src/ofApp.cpp
@@ -28,12 +28,13 @@ void ofApp::draw() {
 
 void ofApp::createEmitters() 
 {
-	auto snowEmitter = std::make_shared<ParticleEmitter>((float)15 * 0.45f, 1000, 0.001);
+	auto snowEmitter = std::make_shared<ParticleEmitter>((float)8, 1000, 0.001);
 	{
 		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(300, 5, 300, 0));
 		posGenerator->pos = glm::vec4{ 0.0, 200.0, 0.0, 0.0 };
 		snowEmitter->addGenerator(posGenerator);
 	}
+
 	system.addEmitter(snowEmitter);
 
 	auto flameEmitter = std::make_shared<ParticleEmitter>((float)1, 500, 0.002);
@@ -45,5 +46,18 @@ void ofApp::createEmitters()
 		flameEmitter->init_velocity_range[1] = glm::vec4(0.15, 0.8, 0.15, 0);
 		flameEmitter->addGenerator(posGenerator);
 	}
+
 	system.addEmitter(flameEmitter);
+
+	auto smokeEmitter = std::make_shared<ParticleEmitter>((float)1, 600, 0.0001);
+	{
+		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(7, 5, 7, 0));
+		posGenerator->pos = glm::vec4{ 0.0, floor_level + 20, 0.0, 0.0 };
+		posGenerator->color = ofColor::dimGrey;
+		smokeEmitter->init_velocity_range[0] = glm::vec4(-0.2, 0.15, -0.2, 0);
+		smokeEmitter->init_velocity_range[1] = glm::vec4(0.2, 0.3, 0.2, 0);
+		smokeEmitter->addGenerator(posGenerator);
+	}
+
+	system.addEmitter(smokeEmitter);
 }
