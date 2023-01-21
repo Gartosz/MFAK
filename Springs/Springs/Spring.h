e8aabb564db033db0459b208167828fefd99e7ed
Bartosz <bartekg001@gmail.com>
2023-01-22 00:40:42 +0100

    Implement constructor.

--- Springs/Springs/Spring.h
+++ Springs/Springs/Spring.h
@@ -6,5 +6,13 @@ class Spring
 public:
 	ofPoint coordinates[2];
 	float length;
+
+	Spring(ofPoint *coordinates)
+	{
+		this->coordinates[0] = coordinates[0];
+		this->coordinates[1] = coordinates[1];
+		length = coordinates[0].distance(coordinates[1]);
+	}
+
 };
 
