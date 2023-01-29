8cfef322e32d0ff7e8af2cdc72766c165f5724ed
Gartosz <bartekg001@gmail.com>
2023-01-29 19:48:30 +0100

    Add new generators.

--- ParticleSystem/ParticleSystem/src/ofApp.cpp
+++ ParticleSystem/ParticleSystem/src/ofApp.cpp
@@ -29,54 +29,60 @@ void ofApp::createEmitters()
 {
 	auto snow_emitter = std::make_shared<ParticleEmitter>((float)3, 1000, 0.001);
 	{
-		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(300, 5, 300, 0));
-		posGenerator->pos = glm::vec4{ 0.0, 200.0, 0.0, 0.0 };
-		snow_emitter->addGenerator(posGenerator);
+		auto pos_generator = std::make_shared<BoxPosGen>(glm::vec4(300, 5, 300, 0));
+		pos_generator->pos = glm::vec4{ 0.0, 200.0, 0.0, 0.0 };
+		snow_emitter->addGenerator(pos_generator);
+		auto velocity_generator = std::make_shared<VelocityRangeGen>(std::vector<glm::vec2>{glm::vec2{-0.01, 0.01}, glm::vec2{ 0.0 }, glm::vec2{ -0.01, 0.01 }, glm::vec2{ 0.0 }});
+		snow_emitter->addGenerator(velocity_generator);
 	}
 
 	system.addEmitter(snow_emitter);
 
-	/*auto flame_emitter = std::make_shared<ParticleEmitter>((float)1, 500, 0.002);
+	auto flame_emitter = std::make_shared<ParticleEmitter>((float)1, 300, 0.002);
 	{
-		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(10, 5, 10, 0));
-		posGenerator->pos = glm::vec4{ 0.0, floor_level, 0.0, 0.0 };
-		posGenerator->color = ofColor::orangeRed;
+		auto pos_generator = std::make_shared<BoxPosGen>(glm::vec4(10, 5, 10, 0));
+		pos_generator->pos = glm::vec4{ 0.0, floor_level, 0.0, 0.0 };
+		pos_generator->color = ofColor::orangeRed;
 		flame_emitter->init_velocity_range[0] = glm::vec4(-0.15, 0.25, -0.15, 0);
 		flame_emitter->init_velocity_range[1] = glm::vec4(0.15, 0.8, 0.15, 0);
-		flame_emitter->addGenerator(posGenerator);
+		flame_emitter->addGenerator(pos_generator);
+		auto velocity_generator = std::make_shared<VelocityRangeGen>(std::vector<glm::vec2>{glm::vec2{ -0.01, 0.01 }, glm::vec2{ -0.02, 0.02 }, glm::vec2{ -0.01, 0.01 }, glm::vec2{ 0.0 }});
+		flame_emitter->addGenerator(velocity_generator);
 	}
 
 	system.addEmitter(flame_emitter);
 
 	auto smoke_emitter = std::make_shared<ParticleEmitter>((float)1, 600, 0.0001);
 	{
-		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(7, 5, 7, 0));
-		posGenerator->pos = glm::vec4{ 0.0, floor_level + 20, 0.0, 0.0 };
-		posGenerator->color = ofColor::dimGrey;
+		auto pos_generator = std::make_shared<BoxPosGen>(glm::vec4(7, 5, 7, 0));
+		pos_generator->pos = glm::vec4{ 0.0, floor_level + 20, 0.0, 0.0 };
+		pos_generator->color = ofColor::dimGrey;
 		smoke_emitter->init_velocity_range[0] = glm::vec4(-0.2, 0.15, -0.2, 0);
 		smoke_emitter->init_velocity_range[1] = glm::vec4(0.2, 0.3, 0.2, 0);
-		smoke_emitter->addGenerator(posGenerator);
+		smoke_emitter->addGenerator(pos_generator);
+		auto velocity_generator = std::make_shared<VelocityRangeGen>(std::vector<glm::vec2>{glm::vec2{ -0.05, 0.05 }, glm::vec2{ -0.02, 0.02 }, glm::vec2{ -0.05, 0.05 }, glm::vec2{ 0.0 }});
+		smoke_emitter->addGenerator(velocity_generator);
 	}
 
 	system.addEmitter(smoke_emitter);
 
 	auto glittering_emitter1 = std::make_shared<ParticleEmitter>((float)1, 100);
 	{
-		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(15, 50, 10, 0));
-		posGenerator->pos = glm::vec4{ 100.0, floor_level + 50.0, 0, 0.0 };
-		posGenerator->color = ofColor::yellow;
-		glittering_emitter1->addGenerator(posGenerator);
+		auto pos_generator = std::make_shared<BoxPosGen>(glm::vec4(15, 50, 10, 0));
+		pos_generator->pos = glm::vec4{ 100.0, floor_level + 50.0, 0, 0.0 };
+		pos_generator->color = ofColor::yellow;
+		glittering_emitter1->addGenerator(pos_generator);
 	}
 
 	system.addEmitter(glittering_emitter1);
 
 	auto glittering_emitter2 = std::make_shared<ParticleEmitter>((float)1, 100);
 	{
-		auto posGenerator = std::make_shared<BoxPosGen>(glm::vec4(15, 50, 10, 0));
-		posGenerator->pos = glm::vec4{ -100.0, floor_level + 50.0, 0, 0.0 };
-		posGenerator->color = ofColor::yellow;
-		glittering_emitter2->addGenerator(posGenerator);
+		auto pos_generator = std::make_shared<BoxPosGen>(glm::vec4(15, 50, 10, 0));
+		pos_generator->pos = glm::vec4{ -100.0, floor_level + 50.0, 0, 0.0 };
+		pos_generator->color = ofColor::yellow;
+		glittering_emitter2->addGenerator(pos_generator);
 	}
 
-	system.addEmitter(glittering_emitter2);*/
+	system.addEmitter(glittering_emitter2);
 }
