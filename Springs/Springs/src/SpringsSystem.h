042af58db64d1a7f9d34f8e7b5fbba205bd6e7b0
Gartosz <bartekg001@gmail.com>
2023-01-30 15:05:29 +0100

    Call function that generates springs.

--- Springs/Springs/src/SpringsSystem.h
+++ Springs/Springs/src/SpringsSystem.h
@@ -14,6 +14,7 @@ public:
 	SpringsSystem(const int &x_count, const int &y_count, const int &x_length, const int y_length, const int &_size = 3, float _k = 0.5)
 	{
 		generatePoints(y_count, x_count, x_length, y_length);
+		generateSprings();
 		size = _size;
 		k = _k;
 	}
