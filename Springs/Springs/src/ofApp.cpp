e737b2ba92aa1ca7d1aad4d4161b6b404ef64668
Bartosz <bartekg001@gmail.com>
2023-02-06 20:27:19 +0100

    Set gui and camera.

--- Springs/Springs/src/ofApp.cpp
+++ Springs/Springs/src/ofApp.cpp
@@ -2,7 +2,16 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	camera.setGlobalPosition({ 0, 40, 0 });
+	camera.setTarget({ 50, 0, 10 });
+	camera.setDistance(600);
+	gui.setup();
+	gui.setSize(250, 120);
+	gui.add(system.dt.setup("Delta time", 0.5, 0.1, 1.15));
+	gui.add(system.ks.setup("Elasticity", 2, 0.1, 15));
+	gui.add(system.kd.setup("Dumping", 7, 0.1, 30));
+	gui.add(system.g.setup("Gravity", -1, -10, 2));
+	gui.add(system.wind.setup("Wind", glm::vec3{0, 0, 0.2}, glm::vec3{-5, -5, -5}, glm::vec3{5, 5, 5}));
 }
 
 //--------------------------------------------------------------
